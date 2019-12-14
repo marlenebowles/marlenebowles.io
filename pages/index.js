7cb2a951107dae99261fff3274b48217525b3146
Computapars <computapars@gmail.com>
2019-12-14 13:52:48 -0500

    Got google fonts in, working on adding pages

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,11 @@
-import React, {Fragment} from 'react'
-import styled from 'styled-components'
-import { Button} from 'grommet';
-import { Notification } from 'grommet-icons';
-
-const Title = styled.h1`
-  font-size: 50px;
-`
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+import { Button, Heading } from 'grommet';
 
 export default () => (
-    <Fragment>
-        <Button icon={<Notification />} onClick={() => {}} />
-        <Title>
-            My page
-        </Title>
-    </Fragment>
-)
+	<Fragment>
+		<Heading level={1} margin="none">
+			Marlene Bowles
+		</Heading>
+	</Fragment>
+);
