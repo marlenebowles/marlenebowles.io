30a8f08abf413b0dc3040dba5f86d44876d87d65
Computapars <computapars@gmail.com>
2019-12-15 12:31:22 -0500

    Adds public file and components file to start styling feature boxes. Changes font

---
+++ components/FeatureBoxes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box, Grid } from 'grommet';
+
+// in an array of content
+// a feature 
+export default () => (
+    <div>
+        header    
+    </div>
+    <div>
+    item 1
+    </div>
+    <div>
+    item 2
+    </div>
+    <div>
+    item 3
+</div>
+);
--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,16 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
-import { Button, Heading } from 'grommet';
+import { Button, Heading, Box } from 'grommet';
 
+const FeatureBox = styled(Box)`
+	background-image: url('/carol-e1449867374669.jpg');
+	height: 70px;
+`;
 export default () => (
 	<Fragment>
 		<Heading level={1} margin="none">
 			Marlene Bowles
 		</Heading>
+		<FeatureBox></FeatureBox>
 	</Fragment>
 );
