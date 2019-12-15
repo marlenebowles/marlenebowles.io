7071723ae3c572609a20f123fbc154f2a01ea1b5
Computapars <computapars@gmail.com>
2019-12-15 14:19:30 -0500

    Adds repeatable rows

--- components/FeatureBoxes.js
+++ components/FeatureBoxes.js
@@ -1,13 +1,32 @@
 import React, { Fragment } from 'react';
+import styled, { css } from 'styled-components';
 import { Box, Grid } from 'grommet';
-import FeatureBoxItem from './FeatureBoxItem';
+const gridRowValues = '500px 400px 350px';
 
-const FeatureBoxes = ({ content }) => (
-	<Fragment>
-		{content.map((item, i) => (
-			<FeatureBoxItem key={i} {...item}></FeatureBoxItem>
-		))}
-	</Fragment>
-);
+const wrapInRepeat = (repeat, values) =>
+	`
+        repeat(${repeat}, ${values});
+    `;
+
+const FeatureBoxLayout = styled.div`
+	display: grid;
+	grid-template-rows: ${props =>
+		props.length && props.length % 4 > 0
+			? wrapInRepeat(props.length % 4, gridRowValues)
+			: gridRowValues};
+`;
+
+const FeatureBoxes = ({ ...props }) => {
+	return (
+		<FeatureBoxLayout length={props.content && props.content.length}>
+			<Box background="#ff7800">cats</Box>
+			<Box background="light-5">dogs</Box>
+			<Box background="light-2">moose</Box>
+			<Box background="light-2">moose</Box>
+			<Box background="light-2">moose</Box>
+			<Box background="light-2">moose</Box>
+		</FeatureBoxLayout>
+	);
+};
 
 export default FeatureBoxes;
