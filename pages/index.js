6673e030a2c7525e89ce879bfd3e379426256ef5
Marlene Bowles <marlenebowles@gmail.com>
2019-12-30 16:50:11 -0500

    Fixes up index page using computapars

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,26 @@ import React from 'react';
 import { FlexBox } from '@computapars/flex';
 import { H1, H2 } from '@computapars/typography';
 export default () => (
-	<FlexBox justifyContent="center" alignContent="center" alignItems="center">
-		<H1 as="h1">Marlene Bowles</H1>
-		<H2 fontFamily="secondary">Coming Soon...</H2>
+	<FlexBox
+		flexDirection="column"
+		height="100vh"
+		style={{
+			background:
+				'linear-gradient(102.77deg, #865ED6 -9.18%, #18BAB9 209.09%)',
+		}}
+		justifyContent="center"
+		alignContent="center"
+		alignItems="center"
+	>
+		<div>
+			<H1 color="white" as="h1">
+				Marlene Bowles
+			</H1>
+		</div>
+		<div>
+			<H2 color="white" mt={0} fontFamily="secondary">
+				Coming Soon...
+			</H2>
+		</div>
 	</FlexBox>
 );
