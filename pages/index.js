463507654d23120442b44823643b514e20bcf54f
Marlene Bowles <marlenebowles@gmail.com>
2019-12-30 19:22:06 -0500

    Added music page and fixed up projects pages to have filtering

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import { FlexBox } from '@computapars/flex';
-import { H1, H2 } from '@computapars/typography';
+import SectionHeader from './../components/SectionHeader';
+import FeatureSection from './../components/FeatureSection';
+import { featuredProjects } from '../schema/';
+import { Container } from '@computapars/layout';
+
 export default () => (
-	<FlexBox
-		flexDirection="column"
-		height="100vh"
-		style={{
-			background:
-				'linear-gradient(102.77deg, #865ED6 -9.18%, #18BAB9 209.09%)',
-		}}
-		justifyContent="center"
-		alignContent="center"
-		alignItems="center"
-	>
-		<div>
-			<H1 color="white" as="h1">
-				Marlene Bowles
-			</H1>
-		</div>
-		<div>
-			<H2 color="white" mt={0} fontFamily="secondary">
-				Coming Soon...
-			</H2>
-		</div>
+	<FlexBox flexDirection="column" height="100vh">
+		<Container>
+			<SectionHeader>Projects.</SectionHeader>
+			<div>View All</div>
+			<FeatureSection content={featuredProjects} />
+		</Container>
+		<Container>
+			<SectionHeader>Music</SectionHeader>
+			<FeatureSection content={featuredProjects} />
+		</Container>
+		<Container>
+			<SectionHeader>About</SectionHeader>
+			<FeatureSection content={featuredProjects} />
+		</Container>
+		<Container>
+			<SectionHeader>Contact</SectionHeader>
+			<FeatureSection content={featuredProjects} />
+		</Container>
 	</FlexBox>
 );
--- pages/projects.js
+++ pages/projects.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { Container } from '@computapars/layout';
-import { H1 } from '@computapars/typography';
 import { featuredProjects } from '../schema/';
 import FeatureSection from '../components/FeatureSection';
+import { FlexBox } from '@computapars/flex';
 
 export default () => (
 	<Container>
-		<H1
-			letterSpacing="xs"
-			style={{
-				fontSize: '140px',
-				zIndex: 1,
-				position: 'relative',
-			}}
-			marginBottom={-26}
-		>
-			Projects.
-		</H1>
+		<FlexBox>
+			<div>All</div>
+			<div>React</div>
+			<div>MERN</div>
+			<div>Node</div>
+		</FlexBox>
 		<FeatureSection content={featuredProjects} />
 	</Container>
 );
