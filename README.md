5af3164460b131a7408855afdcf1c1effaf28cd8
Marlene Bowles <marlenebowles@gmail.com>
2020-01-14 20:09:21 -0500

    Adding a global navigation

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ A portoflio site built with React, Next.Js and the Computapars design system.
 
 http://seanhalpin.io/work
 https://tobiasahlin.com/
+
+## Navigation Page Transition
+
+https://reacttricks.com/animating-next-page-transitions-with-framer-motion/
+
+## Logo
+
+Use Framer to create an MB or M
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
 
+const NavBackground = styled('nav')`
+	position: absolute;
+	top: 2.7vmin;
+	left: 2.8vmin;
+	z-index: 2004;
+	outline: none;
+	width: 23px;
+	padding: 10px;
+`;
 export default () => (
-	<>
+	<NavBackground>
 		<Link href="/">
 			<a>Home</a>
 		</Link>
@@ -12,8 +22,11 @@ export default () => (
 		<Link href="/projects">
 			<a>Projects</a>
 		</Link>
+		<Link href="/music">
+			<a>Music</a>
+		</Link>
 		<Link href="/contact">
 			<a>Contact</a>
 		</Link>
-	</>
+	</NavBackground>
 );
