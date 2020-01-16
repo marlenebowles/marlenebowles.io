04804cd9d4cc441f214bf7420afdfa0d4b76b8a0
Marlene Bowles <marlenebowles@gmail.com>
2020-01-15 21:14:11 -0500

    Added framer motion to library and working on a nav logo animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,48 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 import styled from 'styled-components';
-
+import { FlexBox } from '@computapars/flex';
 const NavBackground = styled('nav')`
 	position: absolute;
 	top: 2.7vmin;
 	left: 2.8vmin;
 	z-index: 2004;
 	outline: none;
-	width: 23px;
+	width: 100%;
 	padding: 10px;
 `;
-export default () => (
+
+const leftStyles = {
+	background: 'transparent',
+	borderTop: '15px solid transparent',
+	borderBottom: '15px solid transparent',
+	borderLeft: '15px solid green',
+	width: 0,
+	height: 0,
+	margin: 'auto',
+};
+const TextStyle = styled('text')`
+	font-family: ${props => props.theme.fonts.primary};
+	font-weight: ${props => props.theme.fontWeights.bold};
+`;
+const rightStyles = {
+	width: 100,
+	height: 100,
+	margin: 'auto',
+};
+
+export const Nav = () => (
 	<NavBackground>
-		<Link href="/">
-			<a>Home</a>
-		</Link>
-		<Link href="/about">
-			<a>About</a>
-		</Link>
-		<Link href="/projects">
-			<a>Projects</a>
-		</Link>
-		<Link href="/music">
-			<a>Music</a>
-		</Link>
-		<Link href="/contact">
-			<a>Contact</a>
-		</Link>
+		<motion.svg style={rightStyles} width="100" height="100">
+			<path fill="#ff7800" d="M 72 60 L 36 60 L 36 0 Z"></path>
+			<path d="M 36 60 L 0 60 L 36 0 Z "></path>
+			<TextStyle fill="white" opacity="0.5" x="20" y="45">
+				M
+			</TextStyle>
+			<TextStyle fill="white" x="40" y="45">
+				B
+			</TextStyle>
+		</motion.svg>
 	</NavBackground>
 );
