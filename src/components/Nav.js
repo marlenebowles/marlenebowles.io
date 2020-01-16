ab0659604127c915633a791e8281b27b97024e5b
Marlene Bowles <marlenebowles@gmail.com>
2020-01-16 13:58:27 -0500

    working with framer motion

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import Link from 'next/link';
 import styled from 'styled-components';
 import { FlexBox } from '@computapars/flex';
+
 const NavBackground = styled('nav')`
 	position: absolute;
 	top: 2.7vmin;
@@ -13,15 +14,6 @@ const NavBackground = styled('nav')`
 	padding: 10px;
 `;
 
-const leftStyles = {
-	background: 'transparent',
-	borderTop: '15px solid transparent',
-	borderBottom: '15px solid transparent',
-	borderLeft: '15px solid green',
-	width: 0,
-	height: 0,
-	margin: 'auto',
-};
 const TextStyle = styled('text')`
 	font-family: ${props => props.theme.fonts.primary};
 	font-weight: ${props => props.theme.fontWeights.bold};
@@ -29,20 +21,46 @@ const TextStyle = styled('text')`
 const rightStyles = {
 	width: 100,
 	height: 100,
-	margin: 'auto',
+	margin: '0 auto',
 };
 
 export const Nav = () => (
 	<NavBackground>
-		<motion.svg style={rightStyles} width="100" height="100">
-			<path fill="#ff7800" d="M 72 60 L 36 60 L 36 0 Z"></path>
-			<path d="M 36 60 L 0 60 L 36 0 Z "></path>
-			<TextStyle fill="white" opacity="0.5" x="20" y="45">
-				M
-			</TextStyle>
-			<TextStyle fill="white" x="40" y="45">
-				B
-			</TextStyle>
-		</motion.svg>
+		<Link href="/">
+			<a>
+				<motion.svg
+					style={rightStyles}
+					whileHover={{ scale: 1.2 }}
+  					whileTap={{ scale: 0.8 }}
+				>
+					<motion.path
+						initial={{
+							opacity: 0,
+							y: 50
+						}}
+						animate={{
+							opacity: 1,
+							y: 0
+						}}
+						d="M 72 60 L 36 60 L 36 0 Z" 
+						fill="var(--animation-left)"
+						opacity="0.5"
+					>
+					</motion.path>
+					<motion.path
+						d="M 36 60 L 0 60 L 36 0 Z"
+						fill="var(--animation-left)"
+					>
+					</motion.path>
+					<TextStyle fill="white" opacity="0.5" x="20" y="45">
+						M
+					</TextStyle>
+					<TextStyle fill="white" x="40" y="45">
+						B
+					</TextStyle>
+					
+				</motion.svg>
+			</a>
+		</Link>
 	</NavBackground>
 );
