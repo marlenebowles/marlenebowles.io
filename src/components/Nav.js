a4d10884d45ab3ec89638c760281f35c86081c23
Marlene Bowles <marlenebowles@gmail.com>
2020-01-16 19:51:36 -0500

    Nicer logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,62 +5,81 @@ import styled from 'styled-components';
 import { FlexBox } from '@computapars/flex';
 
 const NavBackground = styled('nav')`
-	position: absolute;
-	top: 2.7vmin;
-	left: 2.8vmin;
+	position: relative;
 	z-index: 2004;
 	outline: none;
 	width: 100%;
 	padding: 10px;
-`;
-
-const TextStyle = styled('text')`
-	font-family: ${props => props.theme.fonts.primary};
-	font-weight: ${props => props.theme.fontWeights.bold};
+	display: flex;
+	flex-direction: column;
 `;
 const rightStyles = {
-	width: 100,
+	width: 200,
 	height: 100,
 	margin: '0 auto',
 };
 
 export const Nav = () => (
 	<NavBackground>
-		<Link href="/">
-			<a>
-				<motion.svg
-					style={rightStyles}
-					whileHover={{ scale: 1.2 }}
-  					whileTap={{ scale: 0.8 }}
-				>
-					<motion.path
-						initial={{
-							opacity: 0,
-							y: 50
-						}}
-						animate={{
-							opacity: 1,
-							y: 0
-						}}
-						d="M 72 60 L 36 60 L 36 0 Z" 
-						fill="var(--animation-left)"
-						opacity="0.5"
+		<FlexBox justifyContent="center">
+			<Link href="/">
+				<a>
+					<motion.svg
+						style={rightStyles}
+						whileHover={{ y: 5 }}
+						whileTap={{ scale: 0.8 }}
 					>
-					</motion.path>
-					<motion.path
-						d="M 36 60 L 0 60 L 36 0 Z"
-						fill="var(--animation-left)"
-					>
-					</motion.path>
-					<TextStyle fill="white" opacity="0.5" x="20" y="45">
-						M
-					</TextStyle>
-					<TextStyle fill="white" x="40" y="45">
-						B
-					</TextStyle>
-					
-				</motion.svg>
-			</a>
-		</Link>
+						<motion.g>
+							<motion.path
+								animate={{
+									opacity: 1,
+								}}
+								initial={{
+									opacity: 0.6,
+								}}
+								d="M7.62939e-06 -4.45856e-06L8.52723e-06 77L39.3489 77L7.62939e-06 -4.45856e-06Z"
+								fill="#2E4057"
+							/>
+							<motion.path
+								animate={{
+									opacity: 0.6,
+								}}
+								initial={{
+									opacity: 1,
+								}}
+								d="M50.9998 4.48769e-06L11.6509 77L51 77L50.9998 4.48769e-06Z"
+								fill="#2E4057"
+							/>
+						</motion.g>
+						<motion.g
+							initial={{
+								x: 60,
+							}}
+						>
+							<motion.path
+								animate={{
+									opacity: 0.8,
+								}}
+								initial={{
+									opacity: 1,
+								}}
+								d="M38 0L4.48921e-07 1.69198e-05L3.04578e-06 59.4092L38 0Z"
+								fill="#FF6B5E"
+							/>
+							<motion.path
+								animate={{
+									opacity: 0.8,
+								}}
+								initial={{
+									opacity: 1,
+								}}
+								d="M38 76.9998L1.21783e-06 17.5906L3.8147e-06 77L38 76.9998Z"
+								fill="#FF6B5E"
+							/>
+						</motion.g>
+					</motion.svg>
+				</a>
+			</Link>
+		</FlexBox>
 	</NavBackground>
 );
