52c2a4487501372b38ce4d7d8fd2051b59e24a0a
Marlene Bowles <marlenebowles@gmail.com>
2020-01-18 14:24:44 -0500

    Gets a contact and about us page on the repo

--- src/components/ConditionalLink.js
+++ src/components/ConditionalLink.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 
-const ConditionalLink = ({ condition, options, children }) => {
+const ConditionalLink = ({ condition = false, options, children }) => {
 	const { href, as, style } = options;
 	return condition ? (
 		<Link href={href} as={as}>
