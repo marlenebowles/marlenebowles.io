1b75453eb93eeebf109e8544e3f80771d0b231d5
Marlene Bowles <marlenebowles@gmail.com>
2020-01-19 19:54:42 -0500

    Adds consistent padding through app using values in theme

--- src/pages/about.js
+++ src/pages/about.js
@@ -24,6 +24,10 @@ const BackgroundImg = styled.div`
 	z-index: -1;
 	position: absolute;
 	width: 100%;
+	display: none;
+	${props => props.theme.mediaQueries.sm} {
+		display: block;
+	}
 `;
 
 const StyledIconHover = styled(motion.div)`
@@ -34,98 +38,115 @@ export default () => (
 	<FlexBox>
 		<Container>
 			<SectionHeader>About.</SectionHeader>
-			<FlexBox flexDirection="row">
+			<FlexBox flexWrap="wrap" width={1} flexDirection="row">
 				<FlexBox
-					width={[1 / 2]}
-					padding={['30px']}
+					width={[1, 1, 1, 1 / 2]}
 					flexDirection="column"
 					bg="charcoal"
 				>
-					<FlexBox>
-						<Text color="white">
-							I'm an experienced Front-End Developer with over
-							four years working with React and Redux. I have a
-							keen interest in going Full Stack with Node,
-							Express, Python and Django.
-						</Text>
-					</FlexBox>
-					<FlexBox>
-						<Text color="white">
-							You can catch me in Brooklyn, NY where I like to
-							explore music, cooking, cats, and history.
-						</Text>
-					</FlexBox>
-					<FlexBox marginTop={['15px']} flexDirection="column">
-						<H2 margin={['0']} color="white">
-							Contact.
-						</H2>
+					<FlexBox
+						flexDirection="column"
+						padding={['md', 'md', 'xl', 'xl']}
+					>
+						<FlexBox>
+							<Text color="white">
+								I'm an experienced Front-End Developer with over
+								four years working with React and Redux. I have
+								a keen interest in going Full Stack with Node,
+								Express, Python and Django.
+							</Text>
+						</FlexBox>
+						<FlexBox>
+							<Text color="white">
+								You can catch me in Brooklyn, NY where I like to
+								explore music, cooking, cats, and history.
+							</Text>
+						</FlexBox>
+						<FlexBox marginTop={['sm']} flexDirection="column">
+							<H2 margin={['0']} color="white">
+								Contact.
+							</H2>
 
-						<Text color="white">
-							Have a question or want to work together? Please
-							drop me a line via email or social media, I would
-							love to hear from you.
-						</Text>
+							<Text color="white">
+								Have a question or want to work together? Please
+								drop me a line via email or social media, I
+								would love to hear from you.
+							</Text>
 
-						<FlexBox
-							marginTop={['15px']}
-							width={['75%']}
-							alignSelf="center"
-							justifyContent="space-between"
-						>
-							<ConditionalLink
-								options={{
-									href: 'mailto:marlenebowles@gmail.com',
-								}}
+							<FlexBox
+								marginTop={'sm'}
+								width={['75%']}
+								alignSelf="center"
+								justifyContent="space-between"
 							>
-								<StyledIconHover
-									whileHover={{ scale: 1.1 }}
-									whileTap={{ scale: 0.97 }}
+								<ConditionalLink
+									options={{
+										href: 'mailto:marlenebowles@gmail.com',
+									}}
 								>
-									<EmailIcon size={'75px'} fill="white" />
-								</StyledIconHover>
-							</ConditionalLink>
-							<ConditionalLink
-								options={{
-									href: 'https://github.com/marlenebowles',
-								}}
-							>
-								<StyledIconHover
-									whileHover={{ scale: 1.1 }}
-									whileTap={{ scale: 0.97 }}
+									<StyledIconHover
+										whileHover={{ y: -3 }}
+										whileTap={{ scale: 0.97 }}
+									>
+										<EmailIcon size={'55px'} fill="white" />
+									</StyledIconHover>
+								</ConditionalLink>
+								<ConditionalLink
+									options={{
+										href:
+											'https://github.com/marlenebowles',
+									}}
 								>
-									<GithubIcon size={'75px'} fill="white" />
-								</StyledIconHover>
-							</ConditionalLink>
-							<ConditionalLink
-								options={{
-									href:
-										'https://www.linkedin.com/in/marlene-bowles-b1248151/',
-								}}
-							>
-								<StyledIconHover
-									whileHover={{ scale: 1.1 }}
-									whileTap={{ scale: 0.97 }}
+									<StyledIconHover
+										whileHover={{ y: -3 }}
+										whileTap={{ scale: 0.97 }}
+									>
+										<GithubIcon
+											size={'55px'}
+											fill="white"
+										/>
+									</StyledIconHover>
+								</ConditionalLink>
+								<ConditionalLink
+									options={{
+										href:
+											'https://www.linkedin.com/in/marlene-bowles-b1248151/',
+									}}
 								>
-									<LinkedInIcon size={'75px'} fill="white" />
-								</StyledIconHover>
-							</ConditionalLink>
-							<ConditionalLink
-								options={{
-									href:
-										'https://open.spotify.com/user/marleener',
-								}}
-							>
-								<StyledIconHover
-									whileHover={{ scale: 1.1 }}
-									whileTap={{ scale: 0.97 }}
+									<StyledIconHover
+										whileHover={{ y: -3 }}
+										whileTap={{ scale: 0.97 }}
+									>
+										<LinkedInIcon
+											size={'55px'}
+											fill="white"
+										/>
+									</StyledIconHover>
+								</ConditionalLink>
+								<ConditionalLink
+									options={{
+										href:
+											'https://open.spotify.com/user/marleener',
+									}}
 								>
-									<SpotifyIcon size={'75px'} fill="white" />
-								</StyledIconHover>
-							</ConditionalLink>
+									<StyledIconHover
+										whileHover={{ y: -3 }}
+										whileTap={{ scale: 0.97 }}
+									>
+										<SpotifyIcon
+											size={'55px'}
+											fill="white"
+										/>
+									</StyledIconHover>
+								</ConditionalLink>
+							</FlexBox>
 						</FlexBox>
 					</FlexBox>
 				</FlexBox>
-				<FlexBox style={{ position: 'relative' }} width={[1 / 2]}>
+				<FlexBox
+					style={{ position: 'relative' }}
+					width={[1, 1, 1, 1 / 2]}
+				>
 					<BackgroundImg></BackgroundImg>
 				</FlexBox>
 			</FlexBox>
